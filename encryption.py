0eef298a8224d9470699dc0c73af8e130e0ae251
Krishna Kumar <dkrishnakumar987@gmail.com>
2024-07-23 21:26:40 +0530

    Created Encrytion Function

--- encryption.py
+++ encryption.py
@@ -1,7 +1,7 @@
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 
-def encrypt_pwd(password,key):
+def encrypt_pwd(password, key):
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(password.encode('utf8'))
     return ciphertext, cipher.nonce, tag
